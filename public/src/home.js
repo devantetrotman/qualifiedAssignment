28b848acd34607c8e684b20f5ddaed551d1f49e4
Devante Trotman <devantetrotman@gmail.com>
2021-09-03 21:27:31 -0500

    Update home.js

--- public/src/home.js
+++ public/src/home.js
@@ -1,14 +1,99 @@
-function getTotalBooksCount(books) {}
+function getTotalBooksCount(books) {
+  return books.length;
+}
 
-function getTotalAccountsCount(accounts) {}
+function getTotalAccountsCount(accounts) {
+  return accounts.length;
+}
 
-function getBooksBorrowedCount(books) {}
+function getBooksBorrowedCount(books) {
+    let numberBorrowed = 0;
+  for (let j = 0; j < books.length; j++){
+    for (let i = 0; i < books[j].borrows.length; i++){
+      console.log(books[j].borrows[i]);
+      if (books[j].borrows[i].returned == false){
+        numberBorrowed++;
+      }
+    }
+  }
+  return numberBorrowed;
+}
 
-function getMostCommonGenres(books) {}
+function getMostCommonGenres(books) {
+  let counter = {}
+  for (let book of books) {
+    if (counter.hasOwnProperty(book.genre)) {counter[book.genre] = counter[book.genre]+1}
+    else {counter[book.genre]=1}
+    }
 
-function getMostPopularBooks(books) {}
+  let arrayOfCounts = [];
+  for (let [key,value] of Object.entries(counter)) {
+    arrayOfCounts.push({name: key, count: value})
+    }
 
-function getMostPopularAuthors(books, authors) {}
+
+  arrayOfCounts.sort((a,b) => b.count-a.count);
+  console.log(arrayOfCounts);
+  for (let i = 5; i < arrayOfCounts.length; i++){
+  arrayOfCounts.pop(arrayOfCounts[i]);
+  }
+  console.log(arrayOfCounts);
+    return arrayOfCounts;
+  
+  
+}
+
+function getMostPopularBooks(books) {
+  let counter = {}
+  for (let book of books) {
+    counter[book.title] = book.borrows.length
+  }
+
+  let arrayOfCounts = [];
+  for (let [key,value] of Object.entries(counter)) {
+    arrayOfCounts.push({name: key, count: value})
+    }
+
+  arrayOfCounts.sort((a,b) => b.count-a.count);
+  for (let i = arrayOfCounts.length -1; i >= 5; i--){
+    arrayOfCounts.pop();
+  }
+  console.log(arrayOfCounts);
+    return arrayOfCounts;
+  }
+
+  
+  
+
+
+function getMostPopularAuthors(books, authors) {
+  let counter = {}
+  for (let book of books) {
+    for (let author of authors){
+      if (author.id == book.authorId){
+        let authorKey = author.name.first + " " + author.name.last;
+        if (counter.hasOwnProperty(authorKey)) {
+          counter[authorKey] = counter[authorKey]+book.borrows.length}
+        else {counter[authorKey]=book.borrows.length}
+      }
+    }
+}
+  let arrayOfCounts = [];
+  for (let [key,value] of Object.entries(counter)) {
+    arrayOfCounts.push({name: key, count: value})
+    }
+
+
+  arrayOfCounts.sort((a,b) => b.count-a.count);
+  console.log(arrayOfCounts);
+  for (let i = arrayOfCounts.length -1; i >= 5; i--){
+    arrayOfCounts.pop();
+  }
+  console.log(arrayOfCounts);
+    return arrayOfCounts;
+}
+
+
 
 module.exports = {
   getTotalBooksCount,
