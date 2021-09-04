39570192f104f1826165a6b3f4970c471da300e0
Devante Trotman <devantetrotman@gmail.com>
2021-09-03 21:24:36 -0500

    Update accounts.js

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -10,12 +10,48 @@ function findAccountById(accounts, id) {
 }
 
 function sortAccountsByLastName(accounts) {
-
+  let sortedArray = [];
+  sortedArray = accounts.sort((account1, account2) => account1.name.last.toLowerCase() > account2.name.last.toLowerCase() ? 1 : -1
+);
+  return sortedArray;
 }
 
-function getTotalNumberOfBorrows(account, books) {}
+function getTotalNumberOfBorrows(account, books) {
+  let theID = account.id;
+  let timesBorrowed = 0;
+  for (let book in books){
+    let borrowList = books[book].borrows;
+    for (let i = 0; i < borrowList.length; i++){
+      if (borrowList[i].id == theID){
+        timesBorrowed++;
+      }
+    }
+  }
+  return timesBorrowed;
+}
 
-function getBooksPossessedByAccount(account, books, authors) {}
+function getBooksPossessedByAccount(account, books, authors) {
+  let theID = account.id;
+  let bookObj = {};
+  let possessedBooks = [];
+  for (let book in books){
+    let borrowList = books[book].borrows;
+    for (let i = 0; i < borrowList.length; i++){
+      if (borrowList[i].id == theID){
+        for (let author in authors){
+          if (authors[author].id == books[book].authorId){
+            let authorArray = authors[author];
+            console.log(authorArray);
+        possessedBooks.push(Object.assign(bookObj,{id: books[book].id, title: books[book].title, genre: books[book].genre, authorId: books[book].authorId, author: authorArray, borrow: borrowList}));
+      }
+    }
+  }
+      break;
+}
+  }
+  console.log(possessedBooks);
+  return possessedBooks;
+}
 
 module.exports = {
   findAccountById,
